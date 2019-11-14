a465526ed969a3519e541de127927617b6033553
admin <982584989@qq.com>
2019-11-14 14:22:19 +0800

    first commit

---
+++ src/main/java/com/lujieni/apollo/config/MyConfig.java
@@ -0,0 +1,26 @@
+package com.lujieni.apollo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther ljn
+ * @Date 2019/11/14
+ */
+@Configuration
+@Slf4j
+public class MyConfig {
+    @Value("${name}")
+    private String name;
+
+    @Bean
+    public String getString(){
+      log.info(name);
+      return name;
+    }
+
+
+
+}
